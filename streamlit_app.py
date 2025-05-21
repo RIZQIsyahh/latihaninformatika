46785f35c610f20b7372239bbca1801070cab121
RIZQIsyahh <rizqysyahputra3990@gmail.com>
2025-05-21 14:04:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title("Mochammad Syahputra Rizqi")
 st.write(
     "Belajar Seru Bersama Iqi Syah")
-    
+)    
+st.image("https://github.com/RIZQIsyahh/latihaninformatika/blob/main/IMG20250513095950.jpg",width=200)
